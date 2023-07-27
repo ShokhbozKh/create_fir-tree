96e90c47456dcbe712da8a3399b81ca3ebdd37f2
Shohboz <shohboztuit@gmail.com>
2023-07-27 11:18:29 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace project3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for(int i=0; i<n; i++)
+            {
+                for(int j=n; j >=1; j--)
+                {
+                    if ((i>=j))
+                    {
+                        Console.Write("* ");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
